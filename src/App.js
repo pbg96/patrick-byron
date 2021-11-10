ed3f58cb9a99c963807139fc362cf7ed0c141f1c
Patrick Byron Gattoc <82700104+pbg96@users.noreply.github.com>
2021-11-10 17:09:45 +0800

    Made components and added CSS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import Header from './components/header'
+import Hero from './components/hero'
+import Work from './components/work'
+import Contact from './components/contact'
+import Footer from './components/footer'
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Header />
+      <Hero />
+      <Work />
+      <Contact />
+      <Footer />
     </div>
   );
 }

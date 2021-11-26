924f3a02b23b9f79cb2643dddd34c8013f399a66
Patrick Byron Gattoc <82700104+pbg96@users.noreply.github.com>
2021-11-26 16:58:10 +0800

    Added new pages via react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Work from './components/Work'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+import Home from './components/Home'
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import About from'./components/About';
+import WorksList from './components/WorksList';
+import ContactPage from './components/ContactPage';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
+    <Router>
+    <div>  
       <Header />
-      <Hero />
-      <Work />
-      <Contact />
-      <Footer />
+      <Switch>
+      <Route path="/patrick-byron" exact component={Home}/>
+      <Route path="/patrick-byron/about" component={About}/>
+      <Route path="/patrick-byron/works" component={WorksList}/>
+      <Route path="/patrick-byron/contacts" component={ContactPage}/>
+    </Switch>
     </div>
+    </Router>
   );
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,51 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import About from'./About';
+import WorksList from './WorksList';
+import ContactPage from './ContactPage';
+
+
 function Header() {
+    const [isActive, setActive] = useState("false");
+
+    const handleToggle = () => {
+      setActive(!isActive);
+
+    };
     return (
       <div>
           <header className="head">
         <div className="logo button-text">
-            <p>Patrick Byron Gattoc</p>
+            <p><Link to="/patrick-byron" >Patrick Byron Gattoc</Link></p>
         </div>
-        <button className="burger-icon" type="button">
+        <button className="burger-icon" onClick={handleToggle} type="button">
             <i className="fas fa-bars"></i>
         </button>
     </header>
-    <nav className="navlist">
-        <a className="menu-item navlist-item" href=""><h4>About</h4></a>
-        <a className="menu-item navlist-item" href="#works"><h4>Works</h4></a>
-        <a className="menu-item navlist-item" href="#contact"><h4>Contact</h4></a>
+    <div>
+    <nav className={!isActive ? "navlist navlist-expanded" : "navlist"}>
+        <a className="menu-item navlist-item"><Link to="/patrick-byron/about" onClick={handleToggle}><h3>About</h3></Link></a>
+        <a className="menu-item navlist-item"><Link to="/patrick-byron/works" onClick={handleToggle}><h3>Works</h3></Link></a>
+        <a className="menu-item navlist-item"><Link to="/patrick-byron/contacts" onClick={handleToggle}><h3>Contacts</h3></Link></a>
     </nav>
+    </div>
+    
+    <Switch>
+      <Route path="/about">
+      <About />
+      </Route>
+      <Route path="/works">
+      <WorksList />
+      </Route>
+      <Route path="/contacts">
+      <ContactPage/>
+      </Route>
+    </Switch>
       </div>
     );
   }

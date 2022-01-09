177b444cea6c87390d2f2cc16b73e77ab0b1a287
Patrick Byron Gattoc <82700104+pbg96@users.noreply.github.com>
2022-01-09 16:31:30 +0800

    changes in privacy policy

--- src/components/PrivacyPolicy.js
+++ src/components/PrivacyPolicy.js
@@ -6,30 +6,29 @@ function PrivacyPolicy() {
             <p class="body-1">This privacy policy will explain how we use the personal data we collect from you when you use our website.</p><br/>
 
             <p class="body-1">Topics:</p>
-            <ul>
-            <li>What data do we collect?</li>
-            <li>How do we collect your data?</li>
-            <li>How will we use your data?</li>
-            <li>How do we store your data?</li>
-            <li>Marketing</li>
-            <li>What are your data protection rights?</li>
-            <li>What are cookies?</li>
-            <li>How do we use cookies?</li>
-            <li>What types of cookies do we use?</li>
-            <li>How to manage your cookies</li>
-            <li>Privacy policies of other websites</li>
-            <li>Changes to our privacy policy</li>
-            <li>How to contact us</li>
-            <li>How to contact the appropriate authorities</li>
+            <ul class="body-1">
+            <li><a href='#section-1'>What data do we collect?</a></li>
+            <li><a href='#section-2'>How do we collect your data?</a></li>
+            <li><a href='#section-3'>How will we use your data?</a></li>
+            <li><a href='#section-4'>How do we store your data?</a></li>
+            <li><a href='#section-5'>Marketing</a></li>
+            <li><a href='#section-6'>What are your data protection rights?</a></li>
+            <li><a href='#section-7'>What are cookies?</a></li>
+            <li><a href='#section-8'>How do we use cookies?</a></li>
+            <li><a href='#section-9'>What types of cookies do we use?</a></li>
+            <li><a href='#section-10'>How to manage your cookies</a></li>
+            <li><a href='#section-11'>Privacy policies of other websites</a></li>
+            <li><a href='#section-12'>Changes to our privacy policy</a></li>
+            <li><a href='#section-13'>How to contact us</a></li>
             </ul>
 
-            <h5>What data do we collect?</h5><br/>
+            <h5 id="section-1">What data do we collect?</h5><br/>
             <p class="body-2">We collect the following data:</p><br/>
             <ul>
             <li class="body-2">Personal identification information (Name, email address, phone number, etc.)</li>
             </ul><br/>
 
-            <h5>How do we collect your data?</h5><br/>
+            <h5 id="section-2">How do we collect your data?</h5><br/>
             <p class="body-2">You directly provide us  with most of the data we collect. We collect data and process data when you:</p><br/>
             <ul>
             <li class="body-2">Register online or place an order for any of our products or services.</li>
@@ -43,7 +42,7 @@ function PrivacyPolicy() {
             <li class="body-2">Webpages</li>
             </ul><br/>
 
-            <h5>How will we use your data?</h5><br/>
+            <h5 id="section-3">How will we use your data?</h5><br/>
             <p class="body-2">We collect your data so that we can:</p><br/>
             <ul>
             <li class="body-2">Process your order and manage your account.</li>
@@ -53,19 +52,19 @@ function PrivacyPolicy() {
             <p class="body-2">When we process your order, we may send your data to, and also use the resulting information from, credit reference agencies to prevent fraudulent purchases.</p><br/>
 
 
-            <h5>How do we store your data?</h5><br/>
+            <h5 id="section-4">How do we store your data?</h5><br/>
             <p class="body-2">We securely store your data at a secure data cloud server.
             We will keep your personal information for 2 years. Once this time period has expired, we will delete your data by wiping them out on our data cloud server.</p><br/>
 
 
-            <h5>Marketing</h5><br/>
+            <h5 id="section-5">Marketing</h5><br/>
 
             <p class="body-2">We would like to send you information about products and services of ours that we think you might like.
             If you have agreed to receive marketing, you may always opt out at a later date.
             You have the right at any time to stop us from contacting you for marketing purposes.
             If you no longer wish to be contacted for marketing purposes, please contact us.</p><br/>
 
-            <h5>What are your data protection rights?</h5><br/>
+            <h5 id="section-6">What are your data protection rights?</h5><br/>
 
             <p class="body-2">We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:</p><br/>
             <ul>
@@ -79,12 +78,12 @@ function PrivacyPolicy() {
 
             <p class="body-2">If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us at our contact form.</p><br/>
 
-            <h5>Cookies</h5><br/>
+            <h5 id="section-7">Cookies</h5><br/>
 
             <p class="body-2">Cookies are text files placed on your computer to collect standard Internet log information and visitor behavior information. When you visit our websites, we may collect information from you automatically through cookies or similar technology. </p><br/>
             <p class="body-2">For further information, visit allaboutcookies.org.</p><br/>
 
-            <h5>How do we use cookies?</h5><br/>
+            <h5 id="section-8">How do we use cookies?</h5><br/>
 
             <p class="body-2">We use cookies in a range of ways to improve your experience on our website, including:</p><br/>
             <ul>
@@ -92,7 +91,7 @@ function PrivacyPolicy() {
             <li class="body-2">Understanding how you use our website</li>
             </ul><br/>
 
-            <h5>What types of cookies do we use?</h5><br/>
+            <h5 id="section-9">What types of cookies do we use?</h5><br/>
 
             <p class="body-2">There are a number of different types of cookies, however, our website uses:</p><br/>
             <ul>
@@ -100,19 +99,19 @@ function PrivacyPolicy() {
             <li class="body-2">Advertising – We use these cookies to collect information about your visit to our website, the content you viewed, the links you followed and information about your browser, device, and your IP address. We sometimes share some limited aspects of this data with third parties for advertising purposes. We may also share online data collected through cookies with our advertising partners. This means that when you visit another website, you may be shown advertising based on your browsing patterns on our website.</li>
             </ul><br/>
 
-            <h5>How to manage cookies</h5><br/>
+            <h5 id="section-10">How to manage cookies</h5><br/>
 
             <p class="body-2">You can set your browser not to accept cookies, and the above website tells you how to remove cookies from your browser. However, in a few cases, some of our website features may not function as a result.</p><br/>
 
-            <h5>Privacy policies of other websites</h5><br/>
+            <h5 id="section-11">Privacy policies of other websites</h5><br/>
 
             <p class="body-2">The website contains links to other websites. Our privacy policy applies only to our website, so if you click on a link to another website, you should read their privacy policy.</p><br/>
 
-            <h5>Changes to our privacy policy</h5><br/>
+            <h5 id="section-12">Changes to our privacy policy</h5><br/>
 
-            <p class="body-2">We keeps our privacy policy under regular review and place any updates on this web page. This privacy policy was last updated on 22 August 2021.</p><br/>
+            <p class="body-2">We keep our privacy policy under regular review and place any updates on this web page. This privacy policy was last updated on 12 December 2021.</p><br/>
 
-            <h5>How to contact us</h5><br/>
+            <h5 id="section-13">How to contact us</h5><br/>
 
             <p class="body-2">If you have any questions about Our Company’s privacy policy, the data we hold on you, or you would like to exercise one of your data protection rights, please do not hesitate to contact us.</p><br/>
 
